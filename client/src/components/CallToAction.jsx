762ce208c7e2e5f9233a962e7a8b3fc9ddee610f
sapna vishnoi <sapnavishnoi@sapnas-MacBook-Air.local>
2024-06-22 09:51:02 -0700

    Change naming

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -13,7 +13,7 @@ export default function CallToAction() {
                 Checkout these resources with 100 JavaScript Projects
             </p>
             <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
-                <a href="https://www.mdn.com" target='_blank' rel='noopener noreferrer'>
+                <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target='_blank' rel='noopener noreferrer'>
                     100 JavaScript Projects
                 </a>
             </Button>
