122f1280969f1593f92e751629b617c02bcb8bef
sapna vishnoi <sapnavishnoi@sapnas-MacBook-Air.local>
2024-06-21 18:30:59 -0700

    Complete comment dashboard component

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,21 +1,20 @@
-import { Sidebar } from 'flowbite-react'
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { Sidebar } from 'flowbite-react';
 import {
-    HiUser,
-    HiArrowSmRight,
-    HiDocumentText,
-    HiOutlineUserGroup,
-    HiAnnotation,
-    HiChartPie,
-  } from 'react-icons/hi';
-  import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
+  HiUser,
+  HiArrowSmRight,
+  HiDocumentText,
+  HiOutlineUserGroup,
+  HiAnnotation,
+  HiChartPie,
+} from 'react-icons/hi';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userslice';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 export default function DashSidebar() {
-    const location = useLocation();
+  const location = useLocation();
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const [tab, setTab] = useState('');
@@ -41,33 +40,74 @@ export default function DashSidebar() {
       console.log(error.message);
     }
   };
-
- 
   return (
     <Sidebar className='w-full md:w-56'>
-        <Sidebar.Items>
-            <Sidebar.ItemGroup className='flex flex-col gap-1'>
-            <Link to='/dashboard?tab=profile'>
-                <Sidebar.Item active={tab === 'profile'}icon={HiUser} lable={currentUser.isAdmin ? 'Admin':'User'} lableColor='dark' as='div'>
-                    Profile
+      <Sidebar.Items>
+        <Sidebar.ItemGroup className='flex flex-col gap-1'>
+          {currentUser && currentUser.isAdmin && (
+            <Link to='/dashboard?tab=dash'>
+              <Sidebar.Item
+                active={tab === 'dash' || !tab}
+                icon={HiChartPie}
+                as='div'
+              >
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
+          <Link to='/dashboard?tab=profile'>
+            <Sidebar.Item
+              active={tab === 'profile'}
+              icon={HiUser}
+              label={currentUser.isAdmin ? 'Admin' : 'User'}
+              labelColor='dark'
+              as='div'
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          {currentUser.isAdmin && (
+            <Link to='/dashboard?tab=posts'>
+              <Sidebar.Item
+                active={tab === 'posts'}
+                icon={HiDocumentText}
+                as='div'
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+          {currentUser.isAdmin && (
+            <>
+              <Link to='/dashboard?tab=users'>
+                <Sidebar.Item
+                  active={tab === 'users'}
+                  icon={HiOutlineUserGroup}
+                  as='div'
+                >
+                  Users
                 </Sidebar.Item>
-                </Link>
-                {currentUser.isAdmin && (
-                   <Link to='/dashboard?tab=posts'>
-                   <Sidebar.Item active={tab === 'posts'}icon={HiDocumentText}  as='div'>
-                       Posts
-                   </Sidebar.Item>
-                   </Link>
-                )}
-               
-                <Sidebar.Item icon={HiArrowSmRight}
+              </Link>
+              <Link to='/dashboard?tab=comments'>
+                <Sidebar.Item
+                  active={tab === 'comments'}
+                  icon={HiAnnotation}
+                  as='div'
+                >
+                  Comments
+                </Sidebar.Item>
+              </Link>
+            </>
+          )}
+          <Sidebar.Item
+            icon={HiArrowSmRight}
             className='cursor-pointer'
             onClick={handleSignout}
-            >
-                    Sign Out
-                </Sidebar.Item>
-            </Sidebar.ItemGroup>
-        </Sidebar.Items>
+          >
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
     </Sidebar>
-  )
+  );
 }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,10 @@ import { useLocation } from "react-router-dom"
 import DashSidebar from "../components/DashSidebar"
 import DashProfile from "../components/DashProfile"
 import DashPosts from "../components/DashPosts"
+import DashUsers from '../components/DashUsers';
+import DashComments from '../components/DashComments';
+//import DashboardComp from '../components/DashboardComp';
+
 
 export default function Dashboard() {
   const location = useLocation()
@@ -23,6 +27,12 @@ export default function Dashboard() {
       </div>
       {tab === 'profile' && <DashProfile/>}
       {tab === 'posts' && <DashPosts/>}
+       {/* users */}
+       {tab === 'users' && <DashUsers />}
+      {/* comments  */}
+      {tab === 'comments' && <DashComments />}
+      {/* dashboard comp */}
+      
     </div>
   )
 }
